ff184df6650e98b339cc31d6f5e5c28ce30e9b84
torikallcode <torikallcode@gmail.com>
2024-01-05 07:09:43 +0800

    revisi commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./index.html" , "utama.html"],
   theme: {
     extend: {
       fontFamily: {
